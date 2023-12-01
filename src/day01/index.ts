f842bdedf8413e3954c5810f5dba2d910bb0c2bc
andrewbrennanfr <20241062+andrewbrennanfr@users.noreply.github.com>
2023-12-01 11:58:17 +0100

    :white_check_mark: Complete day01 part01

---
+++ src/day01/index.ts
@@ -0,0 +1,34 @@
+export const lines = (input: string): string[] => input.trim().split("\n")
+
+export const log = <T>(value: T, ...args: unknown[]): T =>
+    !Boolean(console.log(value, ...args)) ? value : value
+
+export const matches = (string: string, regex: RegExp): RegExpMatchArray[] => [
+    ...string.matchAll(regex),
+]
+
+export const nth =
+    <T>(number: number): ((array: T[]) => T | undefined) =>
+    (array) =>
+        array[number]
+
+export const sum = (numbers: number[]): number =>
+    numbers.reduce((sum, number) => sum + number, 0)
+
+/* -------------------------------------------------------------------------- */
+
+export const part01 = (
+    input: string,
+    digits = (line: string): string[] =>
+        matches(line, /\d/g).map(nth(0)).map(String)
+): string =>
+    String(
+        sum(
+            lines(input)
+                .map(digits)
+                .map((digits) => `${digits.at(0)}${digits.at(-1)}`)
+                .map(Number)
+        )
+    )
+
+export const part02 = (input: string): string => input
