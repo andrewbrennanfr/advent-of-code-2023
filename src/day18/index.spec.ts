3a576d02f5856107731488d743e3a16e3bc1d36d
andrewbrennanfr <20241062+andrewbrennanfr@users.noreply.github.com>
2023-12-18 20:09:23 +0100

    Writ tests

--- src/day18/index.spec.ts
+++ src/day18/index.spec.ts
@@ -7,12 +7,14 @@ describe("day18", () => {
     describe("part01", () => {
         test("example01", () => expect(part01(example01)).toEqual("62"))
 
-        test("input01", () => expect(part01(input01)).toEqual(""))
+        test("input01", () => expect(part01(input01)).toEqual("56678"))
     })
 
     describe("part02", () => {
-        test.skip("example01", () => expect(part02(example01)).toEqual(""))
+        test.skip("example01", () =>
+            expect(part02(example01)).toEqual("952408144115"))
 
-        test.skip("input01", () => expect(part02(input01)).toEqual(""))
+        test.skip("input01", () =>
+            expect(part02(input01)).toEqual("79088855654037"))
     })
 })
